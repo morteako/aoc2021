ed8572eb23684e45358be29359f8efd3977115e7
Morten Kolstad <morteako@student.matnat.uio.no>
2021-11-01 22:59:17 +0100

    move app from lib to app

--- haskell/src/app/Main.hs
+++ haskell/src/app/Main.hs
@@ -1,8 +1,58 @@
-module Main where
+module Main (
+  main,
+  funcs,
+) where
 
-import qualified Lib
+import CmdArgs
+import Control.Lens
+import Control.Monad (join, void)
+import Data.Bitraversable (Bitraversable (bitraverse))
+import qualified Data.IntMap as Map
+import Data.IntMap.Strict (IntMap)
+import qualified Data.IntMap.Strict as IntMap
+import qualified Day.Day01
+import Input (getInput)
+import Options.Applicative (execParser)
+import Utils ((=:))
+
+funcs :: IntMap (String -> IO (String, String))
+funcs =
+  Map.fromList
+    [ 1 =: Day.Day01.run
+    ]
+
+lastDayNr :: Int
+lastDayRunnner :: String -> IO (String, String)
+(lastDayNr, lastDayRunnner) = IntMap.findMax funcs
+
+runner :: Options -> IO ()
+runner Options{day, input} = do
+  let func :: String -> IO ()
+      func i = case day of
+        LastDay ->
+          lastDayRunnner i >>= void . bitraverse print print
+        SpecificDay d ->
+          case IntMap.lookup d funcs of
+            Nothing -> do
+              putStrLn $ show d <> " is not implemented."
+              putStrLn $ "Currently implemented : " <> unwords (show <$> IntMap.keys funcs)
+            Just dayRunner ->
+              dayRunner i >>= traverseOf_ both putStrLn
+  inputFile <- case input of
+    StdIn -> do
+      getContents
+    File path -> do
+      readFile path
+    Test -> do
+      let path = "input/" <> show lastDayNr <> "test"
+      readFile path
+    DayInput -> do
+      case day of
+        LastDay -> getInput lastDayNr
+        SpecificDay d -> getInput d
+  func inputFile
 
 main :: IO ()
 main = do
-  -- print $ Lib.
-  putStrLn "hello world"
+  let parser = cmdParser lastDayNr
+  execParser parser >>= void . runner
