18e063efca82230c8b6afae9c7a46244521458cd
Morten Kolstad <morteako@student.matnat.uio.no>
2021-11-02 01:21:32 +0100

    added ghcid.sh

---
+++ haskell/ghcid.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+case "$1" in
+    r|R|"")
+    ghcid --command="stack repl :app" -r
+    ;;
+    t|T)
+    case "$2" in
+        "")
+        ghcid -c="stack ghci aoc2021:test aoc2021:lib" -T=':main'
+        ;;
+        *)
+        ghcid -c="stack ghci aoc2021:test aoc2021:lib" -T=':main --match "/Day'$2'/"'
+        ;;
+    esac
+    ;;
+    *)
+    args=${@:1}
+    ghcid --command='stack repl :app' -T=":main $args"
+    ;;
+
+esac
