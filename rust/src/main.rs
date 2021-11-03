e158bdb4cecca365f72f30200d3ec75e43fb45f7
Morten Kolstad <morteako@student.matnat.uio.no>
2021-11-03 13:21:28 +0100

    slightly less horrible rustcode

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -3,13 +3,15 @@ use std::fs;
 
 fn main() {
     let content = fs::read_to_string("../inputs/1").expect("Something went wrong reading the file");
-    let nums = content.lines().map(|x| x.parse::<i32>().unwrap());
-    // let nums2 = ;
-    let mut map: HashMap<i32, i32> = HashMap::with_capacity(200);
-    for num in nums.clone() {
-        map.insert(2020 - num, num);
+    let nums: Vec<i32> = content.lines().map(|x| x.parse::<i32>().unwrap()).collect();
+
+    let mut map: HashMap<i32, i32> = HashMap::with_capacity(nums.len());
+
+    for num in &nums {
+        map.insert(2020 - num, *num);
     }
-    for num in nums.clone() {
+
+    for num in &nums {
         match map.get(&num) {
             Some(a) => {
                 println!("{}", a * num);
@@ -19,15 +21,15 @@ fn main() {
         }
     }
 
-    let mut map2: HashMap<i32, (i32, i32)> = HashMap::with_capacity(200);
+    let mut map2: HashMap<i32, (i32, i32)> = HashMap::with_capacity(nums.len());
 
-    for num in nums.clone() {
-        for num2 in nums.clone() {
-            map2.insert(2020 - num - num2, (num, num2));
+    for num in &nums {
+        for num2 in &nums {
+            map2.insert(2020 - num - num2, (*num, *num2));
         }
     }
 
-    for num in nums.clone() {
+    for num in nums {
         match map2.get(&num) {
             Some((a, b)) => {
                 println!("{}", a * b * num);
