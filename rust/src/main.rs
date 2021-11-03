2c1fbec7384857c858a685ce5a7d6c447e86a5c5
Morten Kolstad <morteako@student.matnat.uio.no>
2021-11-03 01:36:32 +0100

    worst rust code in the world

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,39 @@
+use std::collections::HashMap;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let content = fs::read_to_string("../inputs/1").expect("Something went wrong reading the file");
+    let nums = content.lines().map(|x| x.parse::<i32>().unwrap());
+    // let nums2 = ;
+    let mut map: HashMap<i32, i32> = HashMap::with_capacity(200);
+    for num in nums.clone() {
+        map.insert(2020 - num, num);
+    }
+    for num in nums.clone() {
+        match map.get(&num) {
+            Some(a) => {
+                println!("{}", a * num);
+                break;
+            }
+            None => {}
+        }
+    }
+
+    let mut map2: HashMap<i32, (i32, i32)> = HashMap::with_capacity(200);
+
+    for num in nums.clone() {
+        for num2 in nums.clone() {
+            map2.insert(2020 - num - num2, (num, num2));
+        }
+    }
+
+    for num in nums.clone() {
+        match map2.get(&num) {
+            Some((a, b)) => {
+                println!("{}", a * b * num);
+                break;
+            }
+            None => {}
+        }
+    }
 }
