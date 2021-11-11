17c6835c86c6c76235659532864d96ff5c67ebd9
Morten Kolstad <morteako@student.matnat.uio.no>
2021-11-11 23:03:35 +0100

    remove test suite

--- haskell/src/lib/AoC2015/Day09.hs
+++ haskell/src/lib/AoC2015/Day09.hs
@@ -1,12 +1,12 @@
+{-# LANGUAGE FlexibleContexts #-}
+
 module AoC2015.Day09 where
 
-import Control.Applicative
 import Control.Lens
 import Data.List.Split (splitOn)
 import Data.Map (Map)
 import Data.Map as Map
-import Data.Maybe
-import Data.Semigroup
+import Data.Maybe (fromJust)
 import Test.HUnit ((@=?))
 
 parse :: String -> Map [Char] (Map [Char] Int)
@@ -15,29 +15,28 @@ parse = Map.fromListWith (<>) . concatMap (f . words) . lines
   f [x, "to", y, "=", i] = [(x, Map.singleton y (read @Int i)), (y, Map.singleton x (read @Int i))]
   f xs = error $ show xs
 
-solve :: (forall f. Foldable f => f Int -> Int) -> Map [Char] (Map [Char] Int) -> Int
-solve f m = f $ Map.mapMaybeWithKey (\k _ -> findPath f k m) m
+type FoldCombinator = forall f. Foldable f => Fold (f Int) Int -> (f Int) -> Maybe Int
 
-findPath :: ([Int] -> Int) -> String -> Map [Char] (Map [Char] Int) -> Maybe Int
-findPath f target m | Map.size m == 1 && Map.member target m = Just 0
-findPath f target m =
-  case Map.lookup target m of
-    Nothing -> Nothing
-    Just mm ->
-      case catMaybes . fmap (\(k, v) -> (+ v) <$> findPath f k (Map.delete target m)) $ Map.toList mm of
-        [] -> Nothing
-        xs -> Just $ f xs
+solve :: FoldCombinator -> Map [Char] (Map [Char] Int) -> Int
+solve f m = fromJust $ f folded $ Map.mapMaybeWithKey (\k _ -> findPath k m) m
+ where
+  findPath :: String -> Map [Char] (Map [Char] Int) -> Maybe Int
+  findPath target m | Map.size m == 1 && Map.member target m = Just 0
+  findPath target m =
+    case Map.lookup target m of
+      Nothing -> Nothing
+      Just mm -> f (ifolded . withIndex . to (\(k, v) -> (+ v) <$> findPath k (Map.delete target m)) . _Just) mm
 
 run :: String -> IO (String, String)
 run xs = do
   let parsed = parse xs
-  let resA = solve minimum parsed
+  let resA = solve minimumOf parsed
   print resA
   resA @=? 141
-  let resB = solve maximum parsed
+  let resB = solve maximumOf parsed
   print resB
   resB @=? 736
 
   return mempty
 
--- i : 1.36
+-- i : 1.7s
