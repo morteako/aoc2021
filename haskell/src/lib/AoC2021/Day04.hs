910508ab1ca291d155291474d96c1ec46aa406eb
Morten Kolstad <morteako@student.matnat.uio.no>
2021-12-04 12:22:05 +0100

    4.2

--- haskell/src/lib/AoC2021/Day04.hs
+++ haskell/src/lib/AoC2021/Day04.hs
@@ -2,63 +2,68 @@
 
 module AoC2021.Day04 where
 
-import Control.Lens
-import Data.Char (digitToInt)
-import Data.Digits (unDigits)
-import Data.List (find, sortOn, transpose)
+import Data.List (partition)
 import Data.List.Split
 import Data.Map (Map)
 import qualified Data.Map as Map
+import Data.Set (Set)
 import qualified Data.Set as Set
 import Data.Tuple (swap)
 import Linear
 import Test.HUnit ((@=?))
 import Utils
 
+type Point = (Int, Int)
+
+parse :: [Char] -> ([Int], [Map Int Point])
+parse = f . splitOn "\n\n"
+ where
+  f (draws : boards) = (fmap readInt $ splitOn "," draws, fmap toBoard $ boards)
+  f s = error $ show s
+
+  toBoard = indexedMap . fmap (fmap readInt . words) . lines
+
+indexedMap :: [[Int]] -> Map Int Point
 indexedMap xss = (Map.fromList $ fmap swap q)
  where
   q = [((x, y), w) | (x, xs) <- zip [0 ..] xss, (y, w) <- zip [0 ..] xs]
 
+wins :: [Set Point]
 wins = cs <> fmap (Set.map swap) cs
  where
   cs = fmap addCols [0 .. 4]
   addCols x = Set.fromList $ fmap (x,) [0 .. 4]
 
-parse = f . splitOn "\n\n"
- where
-  f (draws : boards) = (fmap readInt $ splitOn "," draws, fmap toBoard $ boards)
+hasWon :: (a, Set Point) -> Bool
+hasWon (_, curs) = any (`Set.isSubsetOf` curs) wins
 
-  toBoard = indexedMap . fmap (fmap readInt . words) . lines
-
-hasWon (_, curs) = any (\x -> x `Set.isSubsetOf` curs) wins
-
+removeNum :: Int -> (Map Int Point, Set Point) -> (Map Int Point, Set Point)
 removeNum d (m, s)
   | (Just xy, m') <- Map.updateLookupWithKey (\_ _ -> Nothing) d m =
     (m', Set.insert xy s)
   | otherwise = (m, s)
 
--- removeNum d _ = error $ "remove num " ++ show d
-
--- playBingo :: [Int] -> [(Map.Map Int (Int, Int), Set.Set (Int, Int))] -> Map.Map Int (Int, Int)
-playBingo (d : ds) boards
-  | Just winner <- find hasWon played = (d, (d *) $ sum $ Map.keys $ fst winner)
-  | otherwise = playBingo ds played
+playBingo :: (forall a. [a] -> a) -> [Int] -> [(Map Int Point, Set Point)] -> [Int]
+playBingo pick (d : ds) boards
+  | (winners@(_ : _), rest) <- partition hasWon played
+    , let numSum = sum $ Map.keys $ fst $ pick winners =
+    d * numSum : playBingo pick ds rest
+  | otherwise =
+    playBingo pick ds played
  where
   played = fmap (removeNum d) boards
-playBingo [] b = error "play1"
+playBingo pick [] b = []
 
-solveA (nums, boards) = playBingo nums (fmap (,mempty) boards)
-
-solveB = id
+play :: (forall a. [a] -> a) -> ([Int], [Map Int Point]) -> Int
+play pick (nums, boards) = pick $ playBingo pick nums (fmap (,mempty) boards)
 
 run :: String -> IO ()
 run xs = do
   let parsed = parse xs
-  print parsed
-  let resA = solveA parsed
+  let resA = play head parsed
   print resA
+  resA @=? 27027
 
--- print $ wins
-
--- let resB = solveB $ parsed
--- print resB
+  let resB = play last $ parsed
+  print resB
+  resB @=? 36975
